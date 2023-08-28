74304d221d28e3269e67cc5949a3f52f695d61dd
rohitsingh2901 <singhrohitrrr@gmail.com>
2023-08-28 16:30:20 +0530

    update card

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,10 @@ const Home = () => {
   const [Cards, setCards] = useState([]);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [utitle, setuTitle] = useState("");
+  const [udescription, setuDescription] = useState("");
+  const [i, seti] = useState(null);
+
   useEffect(() => {
     fetchCards();
   }, []);
@@ -37,16 +41,16 @@ const Home = () => {
   };
 
 
-  const editCard = async (index) => {
+  const editCard = async () => {
     try {
       const response = await fetch(
-        `http://localhost:5000/cards/edit/${Cards[index]._id}`,
+        `http://localhost:5000/cards/edit/${Cards[i]._id}`,
         {
           method: "PUT",
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ title: title, description: description }),
+          body: JSON.stringify({ title: utitle, description: udescription }),
         }
       );
       const data = await response.json();
@@ -64,6 +68,12 @@ const Home = () => {
   }
   const descriptionChange = (event)=>{
       setDescription(event.target.value);
+  }
+  const utitleChange = (event)=>{
+      setuTitle(event.target.value);
+  }
+  const udescriptionChange = (event)=>{
+      setuDescription(event.target.value);
   }
   const createCard = async(event)=>{
     if(title!=="" && description!==""){
@@ -106,7 +116,10 @@ const Home = () => {
                       onClick={() => deleteCard(i)}
                       className="fa-solid fa-trash mx-2 cursor-pointer"
                     ></i>
-                    <i className="fa-solid fa-pen-to-square mx-2 cursor-pointer"></i>
+                    <i data-toggle="modal"
+            data-target="#exampleModalCenter2" onClick={()=>{seti(i) 
+            setuTitle(Cards[i].title)
+            setuDescription(Cards[i].description)}} className="fa-solid fa-pen-to-square mx-2 cursor-pointer"></i>
                     <i className="fa fa-arrow-right cursor-pointer"></i>
                   </div>
                 </div>
@@ -120,6 +133,75 @@ const Home = () => {
           >
             Create Card <i className="fa-solid fa-plus"></i>
           </h6>
+
+          <div
+            className="modal fade"
+            id="exampleModalCenter2"
+            tabIndex="-1"
+            role="dialog"
+            aria-labelledby="exampleModalCenterTitle"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog modal-dialog-centered" role="document">
+            <form>
+              <div className="modal-content" style={{minWidth:"30rem"}}>
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLongTitle">
+                    Update Card
+                  </h5>
+                  <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Title</label>
+                      <input
+                        required
+                        value={utitle}
+                        type="text"
+                        className="form-control"
+                        onChange={utitleChange}
+                        placeholder="This is a titile"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleInputPassword1">Description</label>
+                      <input
+                        required
+                        value={udescription}
+                        type="text"
+                        className="form-control"
+                        onChange={udescriptionChange}
+                        placeholder="This is a description"
+                      />
+                    </div>
+                  
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    id="cancel"
+                    className="btn btn-secondary"
+                    data-dismiss="modal"
+                  >
+                    Cancel
+                  </button>
+                  <button type="submit" onClick={editCard} {...(utitle && udescription ? { "data-dismiss": "modal" } : {})} className="btn btn-primary">
+                    Update Card
+                  </button>
+                </div>
+              </div>
+              </form>
+            </div>
+          </div>
+
+
 
           <div
             className="modal fade"
